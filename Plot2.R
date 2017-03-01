b5c854eed2ee306fa1cb4d7c4a801f246d6e0a3a
wtq920828 <756554335@qq.com>
2017-03-01 19:27:13 +0800

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,7 @@
+hpc<-read.table("household_power_consumption.txt",header = TRUE,sep=";",na.strings = "?")
+newhpc<-subset(hpc,Date=="1/2/2007"|Date=="2/2/2007")
+datetime <- strptime(paste(newhpc$Date, newhpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(newhpc$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
